804f85a2a8d8cace503fd062f40f71e3eab5bbf6
SenkeSem <sen9morf@gmail.com>
2024-04-01 17:28:33 +0300

    Начал решать 2037

---
+++ 2037. Minimum Number of Moves to Seat Everyone/index.js
@@ -0,0 +1,8 @@
+// const seats = [3, 1, 5];
+// const students = [2, 7, 4];
+
+var minMovesToSeat = function (seats, students) {
+  return 1;
+};
+
+console.log(minMovesToSeat([3, 1, 5], [2, 7, 4]));
