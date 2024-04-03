3953ce83441bfb8f17756edd0309301651569f97
SenkeSem <sen9morf@gmail.com>
2024-04-03 11:29:37 +0300

    2037-done

--- 2037. Minimum Number of Moves to Seat Everyone/index.js
+++ 2037. Minimum Number of Moves to Seat Everyone/index.js
@@ -1,8 +1,14 @@
-// const seats = [3, 1, 5];
-// const students = [2, 7, 4];
-
 var minMovesToSeat = function (seats, students) {
-  return 1;
+  let seatsSort = seats.sort((a, b) => a - b);
+  let studentsSort = students.sort((a, b) => a - b);
+
+  let resultArray = [];
+
+  for (let i = 0, j = 0; i < seatsSort.length; i++, j++) {
+    resultArray.push(Math.abs(seatsSort[i] - studentsSort[j]));
+  }
+
+  return resultArray.reduce((item, acc) => acc + item, 0);
 };
 
-console.log(minMovesToSeat([3, 1, 5], [2, 7, 4]));
+// console.log(minMovesToSeat([3, 1, 5], [2, 7, 4]));
