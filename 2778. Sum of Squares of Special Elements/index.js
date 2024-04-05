d338b17eb55545ee4300f59594be823d6991396a
SenkeSem <sen9morf@gmail.com>
2024-04-05 15:19:58 +0300

    2778-done

--- 2778. Sum of Squares of Special Elements/index.js
+++ 2778. Sum of Squares of Special Elements/index.js
@@ -1,16 +1,13 @@
 var sumOfSquares = function (nums) {
   let result = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    console.log(nums[i]);
-    console.log(result, 'результат');
-
-    if (nums[i] % i == 0) {
-      result += nums[i] * nums[i];
+  for (let i = 1; i <= nums.length; i++) {
+    if (nums.length % i == 0) {
+      result += nums[i - 1] * nums[i - 1];
     }
   }
 
-  // return result;
+  return result;
 };
 
-console.log(sumOfSquares([2, 7, 1, 19, 18, 3]));
+// console.log(sumOfSquares([2, 7, 1, 19, 18, 3]));
