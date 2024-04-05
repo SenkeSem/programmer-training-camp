0a07c2630f9cd91d4b88b238f96817bfcf82fa4c
SenkeSem <sen9morf@gmail.com>
2024-04-05 16:35:32 +0300

    500-start

---
+++ 500. Keyboard Row/index.js
@@ -0,0 +1,9 @@
+var findWords = function (words) {
+  const firstRow = 'qwertyuiop';
+  const secondRow = 'asdfghjkl';
+  const thirdRow = 'zxcvbnm';
+
+  return words;
+};
+
+console.log(findWords(['Hello', 'Alaska', 'Dad', 'Peace']));
