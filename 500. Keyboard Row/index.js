793bbe4bcb1496b50550b689cbf9730f3bfa88c3
SenkeSem <sen9morf@gmail.com>
2024-04-09 16:54:32 +0300

    500-done

--- 500. Keyboard Row/index.js
+++ 500. Keyboard Row/index.js
@@ -3,7 +3,26 @@ var findWords = function (words) {
   const secondRow = 'asdfghjkl';
   const thirdRow = 'zxcvbnm';
 
-  return words;
+  return words.filter((word) => {
+    const arr = word.toLowerCase().split('');
+
+    return (
+      arr.every((char) => firstRow.includes(char)) ||
+      arr.every((char) => secondRow.includes(char)) ||
+      arr.every((char) => thirdRow.includes(char))
+    );
+  });
 };
+
+// const findWords = (words) =>
+//   words.filter((w) => {
+//     const wArr = w.toLowerCase().split(``);
+
+//     return (
+//       wArr.every((c) => `qwertyuiop`.includes(c)) ||
+//       wArr.every((c) => `asdfghjkl`.includes(c)) ||
+//       wArr.every((c) => `zxcvbnm`.includes(c))
+//     );
+//   });
 
 console.log(findWords(['Hello', 'Alaska', 'Dad', 'Peace']));
