944265200b6d7d7ca011d75fdba8589b5e560a18
SenkeSem <sen9morf@gmail.com>
2024-04-16 11:55:11 +0300

    2744-done

---
+++ 2744. Find Maximum Number of String Pairs/index.js
@@ -0,0 +1,15 @@
+var maximumNumberOfStringPairs = function (words) {
+  let count = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    for (let j = i + 1; j < words.length; j++) {
+      if (words[i][0] === words[j][1] && words[i][1] === words[j][0]) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+};
+
+console.log(maximumNumberOfStringPairs(['cd', 'ac', 'dc', 'ca', 'zz']));
