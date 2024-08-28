9ede10bc5be1f6599aa600f554350c7ff2953c24
SenkeSem <sen9morf@gmail.com>
2024-08-28 14:14:20 +0300

    14-refactored

--- 14. Longest Common Prefix/index.js
+++ 14. Longest Common Prefix/index.js
@@ -5,6 +5,8 @@ var longestCommonPrefix = function (strs) {
   for (let i = 1; i < firstWord.length + 1; i++) {
     if (strs.every((word) => word.startsWith(firstWord.slice(0, i)))) {
       prefix += firstWord[i - 1];
+    } else {
+      break;
     }
   }
 
